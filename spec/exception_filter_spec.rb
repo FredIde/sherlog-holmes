437e82a70546970b34328701565a04be0d9dceb4
Ataxexe <ataxexe@devnull.tools>
2015-12-07 10:44:12 -0200

    specs for exception filter

--- spec/exception_filter_spec.rb
+++ spec/exception_filter_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe LogFilter, '#exception' do
+describe LogFilter do
 
   before :all do
     @nullpointer_entry = LogEntry::new exception_class: 'some.bla.NullPointerException'
@@ -8,26 +8,38 @@ describe LogFilter, '#exception' do
     @no_exception_entry = LogEntry::new message: 'Everything went well'
   end
 
-  it 'should filter entries correctly' do
-    filter = LogFilter::exception 'some.bla.NullPointerException'
+  describe '#exception' do
+    it 'should filter entries correctly' do
+      filter = LogFilter::exception 'some.bla.NullPointerException'
 
-    expect(filter.accept? @nullpointer_entry).to be_truthy
-    expect(filter.accept? @generic_exception_entry).to be_falsey
-    expect(filter.accept? @no_exception_entry).to be_falsey
+      expect(filter.accept? @nullpointer_entry).to be_truthy
+      expect(filter.accept? @generic_exception_entry).to be_falsey
+      expect(filter.accept? @no_exception_entry).to be_falsey
+    end
+
+    it 'should expect wildcards' do
+      filter = LogFilter::exception '*NullPointerException'
+
+      expect(filter.accept? @nullpointer_entry).to be_truthy
+      expect(filter.accept? @generic_exception_entry).to be_falsey
+      expect(filter.accept? @no_exception_entry).to be_falsey
+
+      filter = LogFilter::exception 'some.bla*'
+
+      expect(filter.accept? @nullpointer_entry).to be_truthy
+      expect(filter.accept? @generic_exception_entry).to be_truthy
+      expect(filter.accept? @no_exception_entry).to be_falsey
+    end
   end
 
-  it 'should expect wildcards' do
-    filter = LogFilter::exception '*NullPointerException'
+  describe '#exceptions' do
+    it 'should filter entries correctly' do
+      filter = LogFilter::exceptions
 
-    expect(filter.accept? @nullpointer_entry).to be_truthy
-    expect(filter.accept? @generic_exception_entry).to be_falsey
-    expect(filter.accept? @no_exception_entry).to be_falsey
-
-    filter = LogFilter::exception 'some.bla*'
-
-    expect(filter.accept? @nullpointer_entry).to be_truthy
-    expect(filter.accept? @generic_exception_entry).to be_truthy
-    expect(filter.accept? @no_exception_entry).to be_falsey
+      expect(filter.accept? @nullpointer_entry).to be_truthy
+      expect(filter.accept? @generic_exception_entry).to be_truthy
+      expect(filter.accept? @no_exception_entry).to be_falsey
+    end
   end
 
 end
