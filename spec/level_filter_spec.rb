c3e9e68e6334a0b2a503ed1dfa86f2553d56cef1
Ataxexe <ataxexe@devnull.tools>
2015-12-07 00:51:52 -0200

    add more specs to level filter

--- spec/level_filter_spec.rb
+++ spec/level_filter_spec.rb
@@ -7,11 +7,26 @@ describe LogFilter, '#level' do
     @debug_entry = LogEntry::new level: :debug
     @error_entry = LogEntry::new level: :error
 
-    @filter = LogFilter::level :info
+    @info_filter = LogFilter::level :info
+    @debug_filter = LogFilter::level :debug
   end
 
-  it 'should filter only info entries' do
-    expect(@filter.accept? @info_entry).to be_truthy
+  it 'should filter entries correctly' do
+    expect(@info_filter.accept? @info_entry).to be_truthy
+    expect(@info_filter.accept? @debug_entry).to be_falsey
+    expect(@info_filter.accept? @error_entry).to be_falsey
+
+    expect(@debug_filter.accept? @debug_entry).to be_truthy
+    expect(@debug_filter.accept? @info_entry).to be_falsey
+    expect(@debug_filter.accept? @error_entry).to be_falsey
+  end
+
+  it 'should filter ignoring case' do
+    expect(@info_filter.accept? LogEntry::new(level: 'INFO')).to be_truthy
+    expect(@info_filter.accept? LogEntry::new(level: 'Info')).to be_truthy
+
+    expect(@debug_filter.accept? LogEntry::new(level: 'DEBUG')).to be_truthy
+    expect(@debug_filter.accept? LogEntry::new(level: 'Debug')).to be_truthy
   end
 
 end
